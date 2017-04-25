8954b964b00f26dd6475e8e094417bbf6881a55b
Diego Pereira Grassato <dgrassato@ciandt.com>
2017-04-25 13:43:27 -0300

    first commit

--- skeleton/sender.php
+++ skeleton/sender.php
@@ -8,35 +8,49 @@ use PhpAmqpLib\Message\AMQPMessage;
 $connection = new AMQPStreamConnection('127.0.0.1', 5672, 'rabbitmq', 'rabbitmq');
 $channel = $connection->channel();
 
-$queueDefault = "carro";
-$exchange = "carro";
-$queueUsado = "carroUsado";
-$queueNovo = "carroNovo";
-
 // Removendo as filas e exchange para criarmos novamente
-$channel->queue_delete($queueUsado);
-$channel->queue_delete($queueNovo);
-$channel->exchange_delete($exchange);
 
-// Primeiro passo: criar um exchange
-$channel->exchange_declare($exchange, 'topic', false, false, false);
+$channel->queue_delete('SaleCar');
+$channel->queue_delete('Stock');
+$channel->queue_delete('Financial');
+$channel->exchange_delete('car');
 
-// Segundo passo: criar uma fila principal
-$channel->queue_declare($queueDefault, false, true, false, false, false,
+
+// Primeiro passo: criar um exchange(repassa informações para outras filas)
+$channel->exchange_declare('car', 'direct', false, true, false);
+
+// Segundo passo: criar uma fila principal(chegada da venda)
+$channel->queue_declare('SaleCar', false, true, false, false, false,
                         array(
-                          'x-message-ttl' => array('I', 20000),
-                          'x-dead-letter-exchange' => array('S', '_exchange_to_default')
+                          'x-message-ttl' => array('I', 1000),
+                          'x-dead-letter-exchange' => array('S', 'car'),
                         )
 );
-
-$channel->queue_declare($queueUsado, false, true);
-$channel->queue_declare($queueNovo, false, true);
-
+// Terceiro passo: criar as filas
+$channel->queue_declare( 'Stock',
+                         false,
+                         true,
+                         false,
+                          true,
+                          false
+);
+$channel->queue_declare( 'Financial',
+                         false,
+                         true,
+                         false,
+                         true
+);
 
 // Quarto passo: definir que, quando uma mensagem cair no exchange (ou seja, não pode ser processada)
 // ela deverá ser direcionada para a fila de DQL
-$channel->queue_bind($queueUsado, $exchange, $queueDefault);
-$channel->queue_bind($queueNovo, $exchange, $queueDefault);
+$channel->queue_bind('Stock', 'car', 'SaleCar', false);
+$channel->queue_bind('Financial', 'car', 'SaleCar', false);
+
+$data = ['data' => 'Hello'];
+$json = json_encode($data);
+
+$msg = new AMQPMessage($json, ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]);
+$channel->basic_publish($msg, '','SaleCar');
 
 $channel->close();
 $connection->close();
